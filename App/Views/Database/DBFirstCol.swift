0414dc27d4aa4e4c67a00b7927273d955b0532f9
nookery <nooks@qq.com>
2024-03-28 18:23:13 +0800

    自定义进度条

--- App/Views/Database/DBFirstCol.swift
+++ App/Views/Database/DBFirstCol.swift
@@ -18,12 +18,43 @@ struct DBFirstCol: View {
                 
                 if audio.downloadingPercent < 100 {
                     ProgressView(value: audio.downloadingPercent/100)
-                            .progressViewStyle(.circular)
+                        .progressViewStyle(CircularProgressViewStyle(size: 8))
                             .controlSize(.regular)
                 }
             }
 
             // 如果在这里定义了tap事件，会影响table的单击选择功能
+    }
+}
+
+public struct CircularProgressViewStyle: ProgressViewStyle {
+    var size: CGFloat = 20
+    private let lineWidth: CGFloat = 8
+    private let defaultProgress = 0.0
+    private let gradient = LinearGradient(colors: [.purple, .blue], startPoint: .leading, endPoint: .trailing)
+    
+    public func makeBody(configuration: ProgressViewStyleConfiguration) -> some View {
+        ZStack {
+            configuration.label
+            progressCircleView(fractionCompleted: configuration.fractionCompleted ?? defaultProgress)
+            configuration.currentValueLabel
+        }
+    }
+    
+    private func progressCircleView(fractionCompleted: Double) -> some View {
+        Circle()
+            .stroke(gradient, lineWidth: lineWidth)
+            .opacity(0.2)
+            .overlay(progressFill(fractionCompleted: fractionCompleted))
+            .frame(width: size, height: size)
+    }
+    
+    private func progressFill(fractionCompleted: Double) -> some View {
+        Circle()
+            .trim(from: 0, to: CGFloat(fractionCompleted))
+            .stroke(gradient, lineWidth: lineWidth)
+            .frame(width: size)
+            .rotationEffect(.degrees(-90))
     }
 }
 
