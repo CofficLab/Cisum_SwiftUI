14d51452a58972eea72c4cd051bc7b25e66c6c53
nooks <nooks@qq.com>
2024-03-31 21:51:17 +0800

    默认封面增加旋转效果

--- App/Views/DefaultCover.swift
+++ App/Views/DefaultCover.swift
@@ -1,7 +1,9 @@
 import SwiftUI
 
 struct DefaultCover: View {
+    @State private var rotationAngle: Double = 0
     var withBackground: Bool = true
+    var rotate: Bool = false
     var body: some View {
         ZStack {
             HStack {
@@ -12,7 +14,7 @@ struct DefaultCover: View {
             }
         }
         .rotationEffect(.degrees(-90))
-        
+
         .clipShape(withBackground ? RoundedRectangle(cornerSize: CGSize(
             width: 4,
             height: 4
@@ -20,8 +22,19 @@ struct DefaultCover: View {
             width: 0,
             height: 0
         )))
+
+        .rotationEffect(.degrees(rotationAngle))
+        .onAppear { 
+            if rotate {
+                Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
+                    withAnimation {
+                        rotationAngle += 0.3
+                    }
+                }
+            }
+        }
     }
-    
+
     var background: some View {
         ZStack {
             if withBackground {
--- App/Views/Playing/AlbumView.swift
+++ App/Views/Playing/AlbumView.swift
@@ -9,18 +9,20 @@ struct AlbumView: View {
     var image: Image? { getCoverFromDisk() }
 
     var body: some View {
-        if audio.isDownloading {
-            ProgressView(value: audio.downloadingPercent / 100)
-                .progressViewStyle(CircularProgressViewStyle(size: 14))
-                .controlSize(.regular)
-                .scaledToFit()
-        } else if audio.isNotDownloaded {
-            Image(systemName: "arrow.down.circle.dotted").resizable().scaledToFit()
-        } else {
-            if let image = image {
-                image.resizable().scaledToFit()
+        ZStack {
+            if audio.isDownloading {
+                ProgressView(value: audio.downloadingPercent / 100)
+                    .progressViewStyle(CircularProgressViewStyle(size: 14))
+                    .controlSize(.regular)
+                    .scaledToFit()
+            } else if audio.isNotDownloaded {
+                Image(systemName: "arrow.down.circle.dotted").resizable().scaledToFit()
             } else {
-                DefaultCover(withBackground: withBackground)
+                if let image = image {
+                    image.resizable().scaledToFit()
+                } else {
+                    DefaultCover(withBackground: withBackground, rotate: withBackground == false)
+                }
             }
         }
     }
