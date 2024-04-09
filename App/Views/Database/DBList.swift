bf28661e8989b775c1dc8ef05dd84f52c8945d77
nookery <nooks@qq.com>
2024-04-09 11:41:02 +0800

    总量显示在section的标题中

--- App/Views/Database/DBList.swift
+++ App/Views/Database/DBList.swift
@@ -9,29 +9,29 @@ struct DBList: View {
 
     @Query(sort: \Audio.order, animation: .default) var audios: [Audio]
 
-    var total: Int { audioManager.db.getTotal() }
-    var db: DB {audioManager.db}
+    var total: Int { db.getTotal() }
+    var db: DB { audioManager.db }
     var audio: Audio? { audioManager.audio }
 
     var body: some View {
         ZStack {
             VStack(spacing: 0) {
-                List(audios) { audio in
-                    Row(audio)
-                        .listRowInsets(EdgeInsets(top: -0, leading: -20, bottom: 0, trailing: -20))
-                        .listRowSeparator(.visible)
+                List {
+                    Section("共 \(total.description)", content: {
+                        ForEach(audios) { audio in
+                            Row(audio)
+                                .listRowInsets(EdgeInsets(top: -0, leading: -20, bottom: 0, trailing: -20))
+                                .listRowSeparator(.visible)
+                        }
+                    })
                 }
                 .scrollContentBackground(.hidden)
                 .listStyle(.plain)
-
-                if total > 0 {
-                    Text("共 \(total.description)").foregroundStyle(.white)
-                }
             }
+        }
 
-            if audios.count == 0, appManager.flashMessage.isEmpty {
-                DBEmptyView()
-            }
+        if audios.count == 0, appManager.flashMessage.isEmpty {
+            DBEmptyView()
         }
     }
 }
