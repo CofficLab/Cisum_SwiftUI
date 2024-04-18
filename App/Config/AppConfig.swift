00335f31a3c5a283d50253bfa4ae04908c3cad3b
nookery <nooks@qq.com>
2024-04-18 17:40:42 +0800

    Update AppConfig.swift

--- App/Config/AppConfig.swift
+++ App/Config/AppConfig.swift
@@ -63,11 +63,7 @@ extension AppConfig {
     static var rootBackground: some View {
         ZStack {
             Color.black.opacity(0.5).ignoresSafeArea()
-            if debug {
-                BackgroundView.forest
-            } else {
-                BackgroundView.type2A
-            }
+            BackgroundView.type2A
         }
     }
     /// 上半部分播放控制的最小高度
