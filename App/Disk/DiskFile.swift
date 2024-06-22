303ccd584469d627479179b85658bf01f0c2db7c
nooks <nooks@qq.com>
2024-06-22 11:13:25 +0800

    Update DiskFile.swift

--- App/Disk/DiskFile.swift
+++ App/Disk/DiskFile.swift
@@ -2,13 +2,15 @@ import Foundation
 
 struct DiskFile: FileBox {
     var url: URL
-    var isDownloading: Bool
+    var isDownloading: Bool = false
     var isUpdated: Bool = false
     var isDeleted: Bool = false
     var isDownloaded: Bool = true
     var isFolder: Bool = false
-    var downloadProgress: Double
-    var fileName: String
+    var downloadProgress: Double = 1.0
+    var fileName: String = ""
+    var children: [DiskFile]? = nil
+    var index: Int = 0
 }
 
 extension DiskFile {
@@ -29,5 +31,59 @@ extension DiskFile {
             downloadProgress: meta.downloadProgress,
             fileName: meta.fileName!
         )
+    }
+}
+
+// MARK: Children
+
+extension DiskFile {
+    func getChildren() -> [DiskFile]? {
+        let fileManager = FileManager.default
+        
+        do {
+            let contents = try fileManager.contentsOfDirectory(at: url, includingPropertiesForKeys: [.nameKey, .isDirectoryKey], options: .skipsHiddenFiles)
+            var subdirectories = contents.filter { $0.hasDirectoryPath }
+            var files = contents.filter { !$0.hasDirectoryPath }
+            
+            // Sort subdirectories and files by name
+            subdirectories.sort { $0.lastPathComponent < $1.lastPathComponent }
+            files.sort { $0.lastPathComponent < $1.lastPathComponent }
+            
+            var children: [DiskFile] = []
+            
+            for file in files {
+                let fileTree = DiskFile(url: file)
+                children.append(fileTree)
+            }
+            
+            return children.isEmpty ? nil : children
+        } catch {
+            // Handle error
+            return nil
+        }
+    }
+}
+
+// MARK: Next
+
+extension DiskFile {
+    func next() -> DiskFile? {
+        guard let parent = self.parent, let siblings = parent.children, siblings.count > self.index + 1 else {
+            return nil
+        }
+        
+        return siblings[self.index + 1]
+    }
+}
+
+// MARK: Parent
+
+extension DiskFile {
+    var parent: DiskFile? {
+        guard let parentURL = self.url.deletingLastPathComponent() as URL? else {
+            return nil
+        }
+        
+        return DiskFile.fromURL(parentURL)
     }
 }
