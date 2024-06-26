57122482ef2a96979ee38cd58db5c1c8e49ed205
nooks <nooks@qq.com>
2024-06-26 21:12:07 +0800

    Revert "删除视频模式"
    This reverts commit 01fdf7de89d90f4c9a24d018137746a321b43172.

--- App/Data/Disk/DiskScene.swift
+++ App/Data/Disk/DiskScene.swift
@@ -7,6 +7,7 @@ enum DiskScene: String, CaseIterable, Identifiable {
     case Music
     case AudioBook
     case KidsAudio
+    case KidsVideo
     
     var iconName: String {
         switch self {
@@ -25,6 +26,8 @@ enum DiskScene: String, CaseIterable, Identifiable {
             Image(systemName: "figure.and.child.holdinghands")
         case .Music:
             Image(systemName: "music.note.list")
+        case .KidsVideo:
+            Image(systemName: "video.circle")
         case .AudioBook:
             Image(systemName: "books.vertical.circle.fill")
         }
@@ -36,6 +39,8 @@ enum DiskScene: String, CaseIterable, Identifiable {
             "kids_audio"
         case .Music:
             "music"
+        case .KidsVideo:
+            "kids_video"
         case .AudioBook:
             "audio_book"
         }
@@ -47,6 +52,8 @@ enum DiskScene: String, CaseIterable, Identifiable {
             "青少年音频模式"
         case .Music:
             "听歌模式"
+        case .KidsVideo:
+            "青少年视频模式"
         case .AudioBook:
             "有声书模式"
         }
@@ -58,6 +65,8 @@ enum DiskScene: String, CaseIterable, Identifiable {
             "适用于青少年听儿歌、故事等音频内容"
         case .Music:
             "作为歌曲仓库"
+        case .KidsVideo:
+            "适用于青少年看动画片等场景"
         case .AudioBook:
             "适用于听有声书的场景"
         }
