b1a5919f10853ffe519e25958b9191035fd0a3d7
nooks <nooks@qq.com>
2024-06-28 20:48:39 +0800

    Update DirSetting.swift

--- App/Views/Settings/DirSetting.swift
+++ App/Views/Settings/DirSetting.swift
@@ -2,28 +2,41 @@ import SwiftUI
 
 struct DirSetting: View {
     @EnvironmentObject var dataManager: DataManager
-    
+
     var mountedURL: URL? {
         dataManager.disk.getMountedURL()
     }
 
     var body: some View {
         GroupBox {
-            HStack {
-                VStack(alignment: .leading, spacing: 5) {
+            VStack(alignment: .leading, spacing: 5) {
+                HStack {
                     Text("仓库目录").font(.headline)
-                    if dataManager.isiCloudDisk {
-                        Text("☁️ 是 iCloud 云盘目录，会保持同步").font(.footnote)
-                    } else {
-                        Text("💾 是本地目录，不会同步").font(.footnote)
+                    Spacer()
+                    if let url = mountedURL {
+                        BtnOpenFolder(url: url)
+                            .labelStyle(.iconOnly)
                     }
                 }
-                Spacer()
-                
-                if let url = mountedURL {
-                    BtnOpenFolder(url: url)
-                        .labelStyle(.iconOnly)
+                if dataManager.isiCloudDisk {
+                    Text("☁️ 是 iCloud 云盘目录，会保持同步").font(.footnote)
+                } else {
+                    Text("💾 是本地目录，不会同步").font(.footnote)
                 }
+
+                VStack(alignment: .leading) {
+                    ForEach(DiskScene.allCases) { s in
+                        HStack {
+                            Text(s.title)
+                            Spacer()
+                            if let root = dataManager.disk.make(s.folderName)?.root {
+                                BtnOpenFolder(url: root)
+                            }
+                        }
+                    }
+                }
+                .labelStyle(.iconOnly)
+                .padding(.leading)
             }.padding(10)
         }.background(BackgroundView.type1.opacity(0.1))
     }
