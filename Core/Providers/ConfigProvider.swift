597cb401a95579df0774be1b9be54bcc445fc35a
Nookery <nooks@qq.com>
2024-12-24 18:18:15 +0800

    🎨 Chore:  Minor adjustments

---
+++ Core/Providers/ConfigProvider.swift
@@ -0,0 +1,11 @@
+import AVKit
+import Combine
+import Foundation
+import MediaPlayer
+import OSLog
+import SwiftUI
+import MagicKit
+
+class ConfigProvider: NSObject, ObservableObject, AVAudioPlayerDelegate, SuperLog, SuperThread {
+    static let emoji: String = "🔩"
+}
