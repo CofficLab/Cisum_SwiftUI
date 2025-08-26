1e330887363c0695cf7ffbcb50d23321a7e81f7f
Nookery <nooks@qq.com>
2025-08-26 21:55:31 +0800

    📦 PackageUpdate: Update Swift package dependencies

--- Plugins/Audio/AudioService.swift
+++ Plugins/Audio/AudioService.swift
@@ -237,18 +237,18 @@ extension AudioService {
     }
 }
 
-#Preview("Small Screen") {
-    RootView {
-        ContentView()
-    }
-    .frame(width: 500)
-    .frame(height: 1200)
+#Preview("App - Large") {
+    AppPreview()
+        .frame(width: 600, height: 1000)
 }
 
-#Preview("Big Screen") {
-    RootView {
-        ContentView()
-    }
-    .frame(width: 1200)
-    .frame(height: 1200)
+#Preview("App - Small") {
+    AppPreview()
+        .frame(width: 500, height: 800)
 }
+
+#if os(iOS)
+#Preview("iPhone") {
+    AppPreview()
+}
+#endif
