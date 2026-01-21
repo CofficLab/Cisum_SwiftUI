5506be4b7ee41c37f618abc13ed69111ab3c0838
Nookery <nooks@qq.com>
2026-01-21 20:33:35 +0800

    🎨 Chore:  Minor adjustments

--- Core/Providers/PluginProvider.swift
+++ Core/Providers/PluginProvider.swift
@@ -23,7 +23,7 @@ import SwiftUI
 @MainActor
 class PluginProvider: ObservableObject, SuperLog, SuperThread {
     nonisolated static let emoji = "🧩"
-    static let verbose = true
+    static let verbose = false
 
     /// 插件仓库，用于持久化插件配置
     private let repo: PluginRepo
