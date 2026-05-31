9ea4084f665c9e1b9635a2f9b233d74824a30fca
nookery <nooks@qq.com>
2026-06-01 06:11:52 +0800

    Localize main window menu item

--- CisumApp/Core/Bootstrap/BootApp.swift
+++ CisumApp/Core/Bootstrap/BootApp.swift
@@ -45,7 +45,7 @@ struct BootApp: App, SuperLog {
             .defaultSize(width: Config.minWidth, height: Config.defaultHeight)
             .commands {
                 CommandGroup(replacing: .newItem) {
-                    Button("Show Cisum") {
+                    Button(String(localized: "Show Cisum", table: "Core")) {
                         if !AppWindowController.showExistingMainWindow() {
                             openWindow(id: AppWindowController.mainWindowID)
                         }
