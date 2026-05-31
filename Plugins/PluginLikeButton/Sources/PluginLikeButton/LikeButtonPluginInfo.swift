736def081f726b5072d401a4f08ca3829537a9c1
nookery <nooks@qq.com>
2026-06-01 04:18:31 +0800

    Use English source key for like button description

--- Plugins/PluginLikeButton/Sources/PluginLikeButton/LikeButtonPluginInfo.swift
+++ Plugins/PluginLikeButton/Sources/PluginLikeButton/LikeButtonPluginInfo.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 public enum LikeButtonPluginInfo {
-    public static let description = "喜欢/取消喜欢 按钮"
+    public static let description = String(localized: "Like/Unlike Button", table: "LikeButton", bundle: .module)
     public static let iconName = "heart"
     public static let toolbarItemId = "like-toggle"
 }
