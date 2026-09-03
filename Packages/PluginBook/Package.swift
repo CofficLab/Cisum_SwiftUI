d5985ba44b23bfc965cf4d4ea416bb034cc0d2c1
Nookery <nooks@qq.com>
2026-09-03 08:21:18 +0800

    refactor: move PluginBook localization resources to package Resources

--- Packages/PluginBook/Package.swift
+++ Packages/PluginBook/Package.swift
@@ -27,9 +27,10 @@ let package = Package(
                 .product(name: "CisumKernel", package: "CisumKernel"),
                 .product(name: "ProviderStorage", package: "ProviderStorage"),
             ],
-            path: "Sources",
+            path: ".",
+            sources: ["Sources"],
             resources: [
-                .process("Localizable.xcstrings")
+                .process("Resources/Localizable.xcstrings")
             ]
         ),
         .testTarget(
